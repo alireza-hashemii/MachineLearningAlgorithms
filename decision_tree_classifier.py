433cecb10b05051694bbaadfa763ffb428f5fd86
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-06-24 13:45:24 +0330

    Desicion Tree implementation from scratch

--- decision_tree_classifier.py
+++ decision_tree_classifier.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-
 def gini_impurity(labels):
     # when the set is empty , it is considered to be pure
     if len(labels) == 0:
@@ -12,6 +11,111 @@ def gini_impurity(labels):
     return 1 - np.sum(fractions ** 2)
 
 
-def entropy(pos_fraction):
-    entropy_measure = -(pos_fraction * np.log2(pos_fraction) + (1- pos_fraction) * np.log2(1 - pos_fraction))
-    return entropy_measure 
+
+def entropy(labels):
+    if len(labels) == 0:
+        return 0
+
+    counts = np.unique(labels, return_counts=True)[1]
+    fractions = counts / float(len(labels))
+    return - np.sum(fractions * np.log2(fractions))
+
+
+criterion_function = {'gini': gini_impurity, 'entropy': entropy}
+
+def weighted_impurity(groups, criterion='gini'):
+    total = sum(len(group) for group in groups)
+
+    weighted_sum = 0.0
+    for group in groups:
+        weighted_sum += len(group) / float(total) * criterion_function[criterion](group)
+    return weighted_sum
+
+
+def split_node(X, y, index, value):
+    x_index = X[:, index]
+
+   # if this feature is numerical
+    if X[0, index].dtype.kind in ['i', 'f']:
+         mask = x_index >= value
+
+    # if this feature is categorical
+    else:
+            mask = x_index == value
+
+    # split into left and right child
+    left = [X[~mask, :], y[~mask]]
+    right = [X[mask, :], y[mask]]
+    return left, right
+
+
+
+def get_best_split(X, y, criterion):
+    best_index, best_value, best_score, children = None, None, 1, None
+    for index in range(len(X[0])):
+        for value in np.sort(np.unique(X[:, index])):
+                    groups = split_node(X, y, index, value)
+                    impurity = weighted_impurity(
+                                [groups[0][1], groups[1][1]], criterion)
+                    
+    if impurity < best_score:
+                    best_index, best_value, best_score, children = index, value, impurity, groups
+    return {'index': best_index, 'value': best_value,'children': children}
+
+
+
+def get_leaf(labels):
+    # Obtain the leaf as the majority of the labels
+    return np.bincount(labels).argmax()
+
+
+
+def split(node, max_depth, min_size, depth, criterion):
+    """
+    Split children of a node to construct new nodes or assign them terminals
+    @param node: dict, with children info
+    @param max_depth: int, maximal depth of the tree
+    @param min_size: int, minimal samples required to further split a child
+    @param depth: int, current depth of the node
+    @param criterion: gini or entropy
+    """
+    left, right = node['children']
+    del (node['children'])
+    if left[1].size == 0:
+        node['right'] = get_leaf(right[1])
+        return
+    if right[1].size == 0:
+        node['left'] = get_leaf(left[1])
+        return
+    # Check if the current depth exceeds the maximal depth
+    if depth >= max_depth:
+        node['left'], node['right'] = get_leaf(left[1]), get_leaf(right[1])
+        return
+    # Check if the left child has enough samples
+    if left[1].size <= min_size:
+        node['left'] = get_leaf(left[1])
+    else:
+        # It has enough samples, we further split it
+        result = get_best_split(left[0], left[1], criterion)
+        result_left, result_right = result['children']
+        if result_left[1].size == 0:
+            node['left'] = get_leaf(result_right[1])
+        elif result_right[1].size == 0:
+            node['left'] = get_leaf(result_left[1])
+        else:
+            node['left'] = result
+            split(node['left'], max_depth, min_size, depth + 1, criterion)
+    # Check if the right child has enough samples
+    if right[1].size <= min_size:
+        node['right'] = get_leaf(right[1])
+    else:
+        # It has enough samples, we further split it
+        result = get_best_split(right[0], right[1], criterion)
+        result_left, result_right = result['children']
+        if result_left[1].size == 0:
+            node['right'] = get_leaf(result_right[1])
+        elif result_right[1].size == 0:
+            node['right'] = get_leaf(result_left[1])
+        else:
+            node['right'] = result
+            split(node['right'], max_depth, min_size, depth + 1, criterion)
